bb10d23726f98ce9e3f2f9c9798f0c041c9fcf06
andyrod <andyrod209@gmail.com>
2022-01-31 09:06:54 -0800

    Added user input.

--- worksheet.js
+++ worksheet.js
@@ -1,4 +1,13 @@
-let dayOfWeek = 'monday';
-console.log(dayOfWeek);
-dayOfWeek = 'Friday';
-console.log("I can't wait for" + ' ' + dayOfWeek);
+// Days of the week
+    // let or var work to create variables
+// let dayOfWeek = 'monday';
+//     // console.log() is like print() in python
+// console.log(dayOfWeek);
+// dayOfWeek = 'Friday';
+// console.log("I can't wait for" + ' ' + dayOfWeek);
+
+// User Input
+
+let animalInput = prompt('what is your favorite animal? ');
+let colorInput = prompt('What is your favorite color? ');
+console.log("I've never senn a " + colorInput + ' ' + animalInput);
