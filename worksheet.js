023cce7c830441732a6d588771805b7acde3fea9
andyrod <andyrod209@gmail.com>
2022-01-31 13:24:46 -0800

    added a hello and goodbye loop that each repeat 5 times and added a while loop

--- worksheet.js
+++ worksheet.js
@@ -33,26 +33,60 @@
 // }
 
 // Random Number
-let randomNumber = Math.floor(Math.random() *  11);
-// ramdomnumber picks a number from 0 to 10 excludes 11
-let singers;
-// if statements looking for a true value to select singers name
-if (randomNumber >= 0 && randomNumber <= 2){
-    singers = 'Beatles';
-    console.log(singers);
-    }
-else if(randomNumber >= 3 && randomNumber <= 5){
-    singers = 'Stones';
-    console.log(singers);
-    }
-else if(randomNumber >= 6 && randomNumber <= 8){
-    singers = 'Floyd';
-    console.log(singers);
-    }
-else if(randomNumber >= 9 && randomNumber <= 10){
-    singers = 'Tacos';
-    console.log(singers);
-    }
+// let randomNumber = Math.floor(Math.random() *  11);
+// // ramdomnumber picks a number from 0 to 10 excludes 11
+// let singers;
+// // if statements looking for a true value to select singers name
+// if (randomNumber >= 0 && randomNumber <= 2){
+//     singers = 'Beatles';
+//     console.log(singers);
+//     }
+// else if(randomNumber >= 3 && randomNumber <= 5){
+//     singers = 'Stones';
+//     console.log(singers);
+//     }
+// else if(randomNumber >= 6 && randomNumber <= 8){
+//     singers = 'Floyd';
+//     console.log(singers);
+//     }
+// else if(randomNumber >= 9 && randomNumber <= 10){
+//     singers = 'Tacos';
+//     console.log(singers);
+//     }
 
 // Loops
+
 // for Loops
+// for(let i = 0; i < 7; i++){
+//     console.log('javaScript is cool!')
+// }
+
+// loop that displays from 0 to 10
+// let  number = 0
+// for(let i = 0; i < 11; i++){
+//     console.log(number) 
+//     number += 1
+// }
+
+// loop to display hello and then goodbye 5 times each
+// let number = 1 
+// for (let i = 0; i < 10; i++){
+//     if (number == 1 || number == 3 || number == 5 || number == 7 || number == 9){
+//         console.log('hello');
+//     }
+//     else if(number == 2 || number == 4 || number == 6 || number == 8 || number == 10){
+//         console.log('goodbye');
+//     }
+//     number += 1;
+// }
+
+// while loops
+// let fishFlying = true
+// let amountOfFishFlying = 1
+// while(fishFlying){
+//     console.log("That's "+ amountOfFishFlying +" fish flying")
+//     amountOfFishFlying += 1
+//     if(amountOfFishFlying == 6){
+//         fishFlying = false
+//     }
+// }
